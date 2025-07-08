dbf1ec9887bdab0cdbea440571740323fda09d69
goodjjh1234 <goodjjh1234@gmail.com>
2025-07-08 10:26:30 +0900

    FIX: main.py -> CORS 정책에 따른 Domain set

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import router
 
 # TODO: 나중에 리드미 쓸 때 지울 것
@@ -8,6 +9,19 @@ from app.api.v1.router import router
 # fastapi run src/main.py <- 배포 
 
 app = FastAPI()
+
+#cors 허용 도메인
+origins = ["http://localhost",
+           "https://localhost:8000",
+           "http://127.0.0.1:8000"] 
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins, # allow origin info
+  allow_credentials=True, # 인증 정보(cookie, verify header ...etc) allow 여부
+  allow_methods=["*"], #모든 HTTP method allow
+  allow_headers=["*"] #모든 HTTP 헤더 allow
+)
 
 # API 라우터 확장
 app.include_router(router, prefix="/api/v1")
