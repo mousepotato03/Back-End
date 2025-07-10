44f981e982953484fff55f7a3c0aec262bd1d2ba
goodjjh1234 <goodjjh1234@gmail.com>
2025-07-10 20:33:50 +0900

    REFACTOR: rename

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import character, community, leaderBoard, users, verification
+from app.api.v1.endpoints import character, community, leaderboard, users, verification
 
 router = APIRouter()
 router.include_router(character.router, prefix="/character")
 router.include_router(community.router, prefix="/community")
-router.include_router(leaderBoard.router, prefix="/leaderBoard")
+router.include_router(leaderBoard.router, prefix="/leaderboard")
 router.include_router(users.router, prefix="/users")
 router.include_router(verification.router, prefix="/verification")
